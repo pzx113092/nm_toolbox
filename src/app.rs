d80c79e206a25dd5f73a1e64e0bbdc877536be14
olejnik@duck.com <olejnik@duck.com>
2026-06-28 12:00:41 +0200

    initial

---
+++ src/app.rs
@@ -0,0 +1,326 @@
+mod enums;
+use enums::{Unit, Isotope};
+use egui::{CornerRadius, Margin};
+use egui_plot::{Line, Plot, PlotPoints, HoverPosition};
+
+pub struct App {
+    // Converter
+    unit: Unit,
+    conv_input: f32,
+    // Isotope info
+    isotope: Isotope,
+    //Activity calculator
+    cal_date: jiff::civil::Date,
+    cal_time: i32,
+    target_date: jiff::civil::Date,
+    target_time: i32,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        let unit = Unit::MegaBq;
+        let conv_input = 0.0;
+        let isotope = Isotope::Tc99m;
+        let cal_date = d_now();
+        let cal_time = t_now();
+        
+        Self {
+            unit,
+            conv_input,
+            isotope,
+            cal_date,
+            cal_time,
+            target_time: cal_time,
+            target_date: cal_date,
+        }
+    }
+}
+
+impl App {
+    /// Called once before the first frame.
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+
+impl eframe::App for App {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        let visuals = ui.ctx().global_style().visuals.clone();
+        egui::Panel::top("top_panel").show(ui, |ui| {
+            // The top panel is often a good place for a menu bar:
+
+            egui::MenuBar::new().ui(ui, |ui| {
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        egui::CentralPanel::default().show(ui, |ui| {
+            ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+                egui::ScrollArea::vertical()
+                .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::AlwaysHidden)
+                .show(ui, |ui| {
+                    app_frame(&visuals).show(ui, |ui| {converter(self, ui);});
+                    app_frame(&visuals).show(ui, |ui| {isotope_info(self, ui);});
+                    app_frame(&visuals).show(ui, |ui| {calculator(self, ui);});
+                });
+            });
+        });
+    }
+}
+
+fn app_frame(visuals: &egui::Visuals) -> egui::Frame {
+    egui::Frame::default()
+        .stroke(visuals.window_stroke)
+        .inner_margin(Margin::same(10))
+        .shadow(visuals.popup_shadow)
+        .fill(visuals.panel_fill)
+        .corner_radius(CornerRadius::same(16))
+}
+
+fn converter(app: &mut App, ui: &mut egui::Ui) {
+    ui.label(egui::RichText::new("Unit conversion").heading());
+    ui.vertical_centered( |ui| {
+
+        egui::Grid::new("unit_grid")
+        .num_columns(2)
+        .spacing([4.0,4.0])
+        .striped(true)
+        .show(ui, |ui| {
+
+        egui::ComboBox::from_id_salt("unit_combo")
+        .selected_text(app.unit.display())
+        .width(20.0)
+        .show_ui(ui, |ui| {
+            ui.selectable_value(&mut app.unit, Unit::MegaBq, "MBq");
+            ui.selectable_value(&mut app.unit, Unit::GigaBq, "GBq");
+            ui.selectable_value(&mut app.unit, Unit::MicroCi, "µCi");
+            ui.selectable_value(&mut app.unit, Unit::MiliCi, "mCi");
+        });
+        
+        ui.add(egui::DragValue::new(&mut app.conv_input).range(0.0..=1000000.0).max_decimals(4));
+
+        ui.end_row();
+
+        ui.separator();
+        ui.separator();
+        ui.end_row();
+        
+        ui.label("MBq");
+        ui.label(format!("{:.3}", app.conv_input * app.unit.multi() / Unit::MegaBq.multi()));
+        ui.end_row();
+
+        ui.label("GBq");
+        ui.label(format!("{:.3}", app.conv_input * app.unit.multi() / Unit::GigaBq.multi()));
+        ui.end_row();
+
+        ui.label("µCi");
+        ui.label(format!("{:.3}", app.conv_input * app.unit.multi() / Unit::MicroCi.multi()));
+        ui.end_row();
+        
+        ui.label("mCi");
+        ui.label(format!("{:.3}", app.conv_input * app.unit.multi() / Unit::MiliCi.multi()));
+        ui.end_row();
+
+        });
+    });
+}
+
+fn isotope_info(app: &mut App, ui: &mut egui::Ui) {
+    ui.label(egui::RichText::new("Isotope info").heading());
+    ui.vertical_centered( |ui| {
+        egui::Grid::new("isotope_grid")
+        .num_columns(2)
+        .spacing([8.0,4.0])
+        .striped(true)
+        .show(ui, |ui| {
+            
+            isotope_combo(app, ui, "first");
+            ui.end_row();
+            ui.separator();
+            ui.separator();
+            ui.end_row();
+
+            ui.label("Half life:");
+            ui.label(parse_hl(app.isotope.hl().as_secs_f32()));
+            ui.end_row();
+
+            ui.label("Full decay:");
+            ui.label(parse_hl(app.isotope.hl().as_secs_f32() * 10.0));
+            ui.end_row();
+        });
+        ui.separator();
+
+        
+        if app.conv_input > 0.0 {
+            let d = app.isotope.hl().as_secs_f32() / 10.0;
+            let pp: PlotPoints<'_> = (0..100).map(|i| {
+                let x = i as f32 * d;
+                [x as f64, activity_left(app.conv_input, app.isotope.hl().as_secs_f32(), x) as f64]
+            }).collect();
+            
+            let line = Line::new("activity", pp);
+
+            Plot::new("Activity_plot")
+                .allow_drag(false)
+                .allow_scroll(false)
+                .width(ui.available_width())
+                .view_aspect(2.0)
+                .show_grid(false)
+                .x_axis_label("[s]")
+                .y_axis_label(format!("[{}]", app.unit.display()))
+                .label_formatter(|pos| match pos {
+                    HoverPosition::NearDataPoint { plot_name, position, .. } if !plot_name.is_empty() => {
+                        Some(format!("Activity: {:.1} {} ({:.1}%)\nTime passed: {}",
+                                position.y, 
+                                app.unit.display(), 
+                                position.y * 100.0 / app.conv_input as f64, 
+                                parse_hl(position.x as f32)))}
+                        _ => None,
+                })
+                .show(ui, |plot_ui| plot_ui.line(line));
+        } else {
+            let line = Line::new("activity", PlotPoints::default());
+            Plot::new("Activity_plot")
+            .allow_drag(false)
+            .allow_scroll(false)
+            .width(ui.available_width())
+            .view_aspect(2.0)
+            .show_grid(false)
+            .x_axis_label("[s]")
+            .y_axis_label(format!("[{}]", app.unit.display()))
+            .show(ui, |plot_ui| plot_ui.line(line));
+        }
+    });
+}
+
+fn calculator(app: &mut App, ui: &mut egui::Ui) {
+    ui.label(egui::RichText::new("Activity calculator").heading());
+    ui.vertical_centered( |ui| {
+        egui::Grid::new("activity_calculator")
+        .num_columns(2)
+        .spacing([8.0,4.0])
+        .striped(true)
+        .show(ui, |ui| {
+            isotope_combo(app, ui, "second");
+            ui.end_row();
+            
+            ui.separator();
+            ui.separator();
+            ui.end_row();
+
+            ui.label("Calibration activity:");
+            ui.add(egui::DragValue::new(&mut app.conv_input).range(0.0..=1000000.0).max_decimals(4));
+            ui.end_row();
+
+            ui.label("Calibration: ");
+            ui.horizontal(|ui|{
+                ui.add(egui_extras::DatePickerButton::new(&mut app.cal_date).id_salt("cal_datepicker"));
+                if ui.button("today").clicked() { app.cal_date = d_now() }
+                time_picker(ui, &mut app.cal_time);
+                if ui.button("now").clicked() { app.cal_time = t_now() }
+            });
+            ui.end_row();
+
+            ui.label("Target:");
+            ui.horizontal(|ui|{
+                ui.add(egui_extras::DatePickerButton::new(&mut app.target_date).id_salt("target_datepicker"));
+                if ui.button("today").clicked() { app.target_date = d_now() }
+                time_picker(ui, &mut app.target_time);
+                if ui.button("now").clicked() { app.target_time = t_now() }
+            });
+            ui.end_row();
+            ui.label("Activity @ target:");
+            //ui.label()
+            let cal_t = i32_to_hms(app.cal_time);
+            let tar_t = i32_to_hms(app.target_time);
+            let span = app.target_date.to_datetime(tar_t) - app.cal_date.to_datetime(cal_t);
+            let span_f = span.total(jiff::Unit::Second).unwrap_or(0.0);
+            ui.label(format!("{:.4}", activity_left(app.conv_input, app.isotope.hl().as_secs_f32(), span_f as f32)));
+
+        });
+    });
+}
+
+fn isotope_combo(app: &mut App, ui: &mut egui::Ui, name: &str) {
+    egui::ComboBox::from_id_salt(name)
+            .selected_text(app.isotope.display())
+            .width(20.0)
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut app.isotope, Isotope::Tc99m, Isotope::Tc99m.display());
+                ui.selectable_value(&mut app.isotope, Isotope::I131, Isotope::I131.display());
+                ui.selectable_value(&mut app.isotope, Isotope::I123, Isotope::I123.display());
+                ui.selectable_value(&mut app.isotope, Isotope::Lu177, Isotope::Lu177.display());
+            });
+}
+
+// duration in seconds
+fn parse_hl(duration: f32) -> String {
+    if duration >= 86400.0 {
+        format!("{:.2} days", duration / 86400.0)
+    } else if duration >= 7200.0 {
+        format!("{:.2} hours", duration / 3600.0)
+    } else if duration >= 120.0 {
+        format!("{:.2} minutes", duration / 60.0)
+    } else {
+        format!("{duration:.2} seconds")
+    }
+}
+
+fn activity_left(n0: f32, hl: f32, t: f32) -> f32 {
+    if hl != 0.0 {
+        n0 * f32::powf(0.5, t / hl)
+    } else {
+        0.0
+    }
+}
+
+fn time_picker(ui: &mut egui::Ui, time: &mut i32) {
+    ui.horizontal(|ui| {
+        
+        //let mut hour_str = format!("{:02}", &app.cal_time.0);
+        //let mut h_buf = egui::TextBuffer::insert_text(&mut self, text, char_index)
+        ui.add(egui::DragValue::new(time)
+    .range(0..=((60 * 60 * 24) - 1))
+    .custom_formatter(|n, _| {
+        let n = n as i32;
+        let hours = n / (60 * 60);
+        let mins = (n / 60) % 60;
+        let secs = n % 60;
+        format!("{hours:02}:{mins:02}:{secs:02}")
+    })
+    .custom_parser(|s| {
+        let parts: Vec<&str> = s.split(':').collect();
+        if parts.len() == 3 {
+            parts[0].parse::<i32>().and_then(|h| {
+                let m = parts[1].parse::<i32>()?;
+                parts[2].parse::<i32>().map(|s| {
+                                        ((h * 60 * 60) + (m * 60) + s) as f64
+                                    })
+            })
+            .ok()
+        } else {
+            None
+        }
+    }));
+    });
+
+}
+
+fn t_now() -> i32 {
+    let now = (jiff::Zoned::now().hour() as i32, jiff::Zoned::now().minute() as i32, jiff::Zoned::now().second() as i32);
+    now.0 * 3600 + now.1 * 60 + now.2
+}
+
+fn d_now() -> jiff::civil::Date {
+    jiff::Zoned::now().date()
+}
+
+fn i32_to_hms(s: i32) -> jiff::civil::Time {
+    let mut sec = s;
+    let hours = sec / 3600;
+    sec -= hours * 3600;
+    let minutes = sec / 60;
+    sec -= minutes * 60;
+
+    jiff::civil::time(hours as i8, minutes as i8, sec as i8, 0)
+}
---
+++ src/app/enums.rs
@@ -0,0 +1,73 @@
+use std::time::Duration;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Unit {
+    MegaBq,
+    GigaBq,
+    MicroCi,
+    MiliCi,
+}
+
+impl Unit {
+    pub fn multi(&self) -> f32 {
+        match self {
+            Self::MegaBq => 1.0,
+            Self::GigaBq => 1000.0,
+            Self::MicroCi => 0.037,
+            Self::MiliCi => 37.0,
+        }
+    }
+
+    pub fn display(&self) -> &str {
+        match self {
+            Self::MegaBq => "MBq",
+            Self::GigaBq => "GBq",
+            Self::MicroCi => "µCi",
+            Self::MiliCi => "mCi",
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Isotope {
+    Tc99m,
+    I131,
+    I123,
+    Lu177,
+}
+
+impl Isotope {
+    pub fn hl(&self) -> Duration {
+        match self {
+            Self::Tc99m => Duration::from_secs_f32(21624.76),
+            Self::I131 => Duration::from_secs_f32(692928.0),
+            Self::I123 => Duration::from_secs_f32(47602.8),
+            Self::Lu177 => Duration::from_secs_f32(578880.0),
+        }
+    }
+
+    pub fn display(&self) -> egui::text::LayoutJob {
+        let mut job = egui::text::LayoutJob::default();
+        let style = egui::style::Style::default();
+
+        let tx: (&str, &str) = match self {
+            Self::Tc99m => ("99m","Tc"),
+            Self::I131 => ("131", "I"),
+            Self::I123 => ("123", "I"),
+            Self::Lu177 => ("177", "Lu"),
+        };
+    
+        egui::RichText::new(tx.0)
+            .strong()
+            .small()
+            .color(style.visuals.text_color())
+            .append_to(&mut job, &style, egui::FontSelection::Default, egui::Align::Min);
+
+        egui::RichText::new(tx.1)
+            .strong()
+            .color(style.visuals.text_color())
+            .append_to(&mut job, &style, egui::FontSelection::Default, egui::Align::Min);
+
+        job
+    }
+}
